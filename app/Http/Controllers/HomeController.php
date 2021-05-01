1b03f575959c3d129a2c5c55b26fc0885e2a0bff
Ali <alzaeem.ali.89@gmail.com>
2021-05-01 16:59:48 +0300

    add slider and edit pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\customer;
+use App\Models\event;
+use App\Models\Goal;
+use App\Models\slider;
+use App\Models\team;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $teams = team::all();
+        $customers = customer::paginate(4);
+        $events = event::paginate(4);
+        foreach($events as $event)
+        {
+            $event->description = substr($event->description,0,50) . '...';
+        }
+        $sliderData = slider::all();
+        $goalsData = Goal::paginate(6);
+        foreach($goalsData as $item)
+        {
+            $item->description = substr($item->description,0,50) . '...';
+        }
+        return view('index', ['slider_data' => $sliderData, 'goals_data' => $goalsData, 'events' => $events,
+        'customers' => $customers, 'teams' => $teams]);
+    }
+
+    public function showItem($id)
+    {
+        $sliderData = slider::all();
+        $goalsData = Goal::all();
+        $item = Goal::find($id);
+        return view('showgoals', ['slider_data' => $sliderData, 'goals_data' => $goalsData,'goalitem' => $item]);
     }
 }
